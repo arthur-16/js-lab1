d03666b6178a1581db37da15ba0bdd287d817972
arthur16 <artbscott16@gmail.com>
2018-05-10 16:36:22 -0500

    first commit

---
+++ lab1.js
@@ -0,0 +1,34 @@
+var friends = ['Steve', 'James', 'Greg', 'Lee', 'Phil'];
+
+    for(var i = 0; i < friends.length; i++)  {
+        for(var j=99; j > 0; j--) {
+            if(j>2){
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file ");
+            } else if(j ===2) {
+                console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes me out, clears it all out, " + (j-1) + " line of code in the file ");
+            } else {
+                console.log(" 1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file ");
+            }
+            
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
